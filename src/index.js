4c0e256e09488e6b890739e13664ab5c60e5a5db
Christian Hansen <ChrisWcs@users.noreply.github.com>
2019-02-18 12:51:13 -0700

    Somewhat finished

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function App() {
+const App = () => {
+  const [r, setR] = React.useState(100);
+  const [b, setB] = React.useState(100);
+  const [g, setG] = React.useState(100);
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <div style={{
+         display: 'flex',
+         justifyDirection: 'row',
+      }}>
+        <div>
+          <h4>Red Value</h4>
+          <input value={r} onChange={e => {setR(e.target.value)}}/>
+        </div>
+        <div>
+          <h4>Blue Value</h4>
+          <input value={b} onChange={e => { setB(e.target.value) }} />
+        </div>
+        <div>
+          <h4>Green Value</h4>
+          <input value={g} onChange={e => { setG(e.target.value) }} />
+        </div>
+      </div>
+      <div style={{
+        height: 250,
+        width: 250,
+        background: `rgb(${r},${b},${g})`
+      }}/>
+    </div>
   );
 }
 
